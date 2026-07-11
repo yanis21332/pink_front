fd69543f9f2f32c01de819888377afc44aeb21bd
yanis21332 <66674136+yanis21332@users.noreply.github.com>
2026-07-11 15:12:15 +0400

    Update Register.js

--- components/Register.js
+++ components/Register.js
@@ -1,10 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { AUTH_COOKIE_NAME } from "../lib/auth";
 import { loginUser } from "../lib/api";
-import { useState, useEffect } from "react";
 
 const AuthPage = styled.main`
   min-height: 100vh;
